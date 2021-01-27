9dda344498ba5df1a1300aacef43500c82a4bfb9
giuseppecolucci <giuseppecolucciwork@gmail.com>
2021-01-27 15:12:23 +0100

    controllo

---
+++ main.js
@@ -0,0 +1,31 @@
+new Vue({
+    el:'#app',
+    data:{
+
+        //array di immagini
+        images:[
+            {img:"https://images.pexels.com/photos/371633/pexels-photo-371633.jpeg?cs=srgb&dl=clouds-country-daylight-371633.jpg&fm=jpg"
+            },
+            {img:"https://images.pexels.com/photos/371633/pexels-photo-371633.jpeg?cs=srgb&dl=clouds-country-daylight-371633.jpg&fm=jpg" 
+            
+            },
+            {img:"https://static.photocdn.pt/images/articles/2017/04/28/iStock-646511634.jpg",
+            
+            },
+            {img:"https://cdn.mos.cms.futurecdn.net/FUE7XiFApEqWZQ85wYcAfM.jpg",
+            
+            },
+         
+            {img: "https://static.photocdn.pt/images/articles/2017/04/28/iStock-546424192.jpg"}
+      
+
+        ],
+        
+    },
+    methods:{
+
+    }
+
+
+
+});
