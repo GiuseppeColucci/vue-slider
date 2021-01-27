466877aceda62f9760a88a2a8065547642a4bd21
giuseppecolucci <giuseppecolucciwork@gmail.com>
2021-01-27 15:39:28 +0100

    controllo 3

--- main.js
+++ main.js
@@ -9,10 +9,10 @@ new Vue({
             "https://images.pexels.com/photos/371633/pexels-photo-371633.jpeg?cs=srgb&dl=clouds-country-daylight-371633.jpg&fm=jpg" 
             
             ,
-           "https://static.photocdn.pt/images/articles/2017/04/28/iStock-646511634.jpg",
+           "https://static.photocdn.pt/images/articles/2017/04/28/iStock-646511634.jpg"
             
             ,
-            "https://cdn.mos.cms.futurecdn.net/FUE7XiFApEqWZQ85wYcAfM.jpg",
+            "https://cdn.mos.cms.futurecdn.net/FUE7XiFApEqWZQ85wYcAfM.jpg"
             
             ,
          
@@ -22,8 +22,17 @@ new Vue({
         ],
         
     },
+    // variabile contatore per lo slider
+    sliderGo:0,
     methods:{
 
+        prev:function(){
+            this.sliderGo-=1
+        },
+
+        next:function(){
+            this.sliderGo+=1
+        }
     }
 
 
